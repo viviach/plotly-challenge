1b68af830b83c5b6066b01f09e1e6f6b6a210761
viviach <viviana.aragonch@gmail.com>
2021-07-01 11:38:18 -0500

    updated script

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+// const url= "https://github.com/viviach/plotly-challenge/blob/main/samples.json";
+
+// d3.json(url).then((Data) => {
+//     console.log(Data);  
+//     });
+
+d3.json("plotly-challenge/samples.json").then((Data) => {
+    console.log(Data);  
+    });
