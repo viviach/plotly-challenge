c34e1b8380deb3abc0d73210c13a4c9dd2b56d23
viviach <viviana.aragonch@gmail.com>
2021-07-03 13:56:10 -0500

    bar char yaxis label modified

--- static/js/app.js
+++ static/js/app.js
@@ -4,10 +4,10 @@ d3.json("data/samples.json").then(function(data) {
 });
   
 // On change to the DOM, call getData()
-d3.selectAll("#selDataset").on("change",barchar);
+d3.selectAll("#selDataset").on("change",chars);
 
 //we will create a function to get an ID from a dropdown menu
-function barchar() {
+function chars() {
     d3.json("data/samples.json").then(function(data) {
         var names =Object.values(data.names);
         //define the Menu
@@ -58,13 +58,16 @@ function barchar() {
         if (selectedOption===id_value){
             //console.log (id)
             var data_sample=[id_value, slicedOtu_ids, reversedSample_values, slicedOtu_labels]
-            console.log(data_sample) 
+            console.log(data_sample);
+            var y= slicedOtu_ids.map(otu => "OTU " + otu).reverse();
             function init() {
                 data_bar = [{
                    x: data_sample[2],
-                   y: data_sample[3],
+                   y: y, 
                    type:"bar",
+                   text: data_sample[3],
                    orientation: "h"
+                   
                }];
 
                var layout_bar = {
@@ -90,6 +93,7 @@ function barchar() {
 
                 var layout_bubble = {
                 title: "Bacteria Cultures Per Sample",
+                
                 };
 
                 var CHART_bubble = d3.selectAll("#bubble").node();
@@ -105,76 +109,8 @@ function barchar() {
     });
 };
 
-barchar()
+chars()
 
 
 
 
-
-
-// /**
-//  * samples (object of 4 arrays)
-//  * index 0 - id
-//  * index 1 - otu_ids
-//  * index 2 - sample_values
-//  * index 3 - otu_labels
-//  */
-
- 
-// d3.json("data/samples.json").then(function(data) {
-//    // retrieve the data for each id
-//     var samples =Object.values(data.samples);
-//     // console.log(samples)
-//     samples.forEach((i)=>{
-//         var id=Object.values(i); // for each i
-//         console.log (id);
-//         //retrieve id
-//         var id_value=id[0];
-//         //print the result
-//         console.log(id_value);
-//         //retrieve otu_ids
-//         var otu_ids=Object.values(id[1]);
-//         // Slice the first 10 objects for plotting
-//         var slicedOtu_ids = otu_ids.slice(0, 10);
-//         //print the result
-//         console.log (slicedOtu_ids);
-//         //retrieve sample_values
-//         var sample_values=Object.values(id[2]);
-//         // Slice the first 10 objects for plotting
-//         var slicedSample_values = sample_values.slice(0, 10);
-//         //print the result
-//         console.log (slicedSample_values);
-//         //retrieve otu_labels
-//         var otu_labels=Object.values(id[3]);
-//         // Slice the first 10 objects for plotting
-//         var slicedOtu_labels = otu_labels.slice(0, 10);
-//         //print the result
-//         console.log (slicedOtu_labels);
-//      });  
-// });
-
-// // // We will set a funtion to get the data when an option is selected
-// function getData() {
-//     var dropdownMenu = d3.select("#selDataset");
-//     // Assign the value of the dropdown menu option to a variable
-//     var id = dropdownMenu.property("value");
-//     // Initialize an empty array for the country's data
-//     var id_data = [];
-  
-//     if (id == id_value) {
-//         id_data = 1;
-//     }
-//     else  {
-//         id_data = 0;
-//     }
-    
-//   }
-
-
-
-
-
-//Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-// Use sample_values as the values for the bar chart.
-// Use otu_ids as the labels for the bar chart.
-// Use otu_labels as the hovertext for the chart.
